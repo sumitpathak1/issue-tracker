11b25ff0ed06442eaff929c0b3ff9dac01453fb9
Sumit Pathak <alphainteractivestudio@gmail.com>
2024-01-03 15:48:33 +0530

    First commit

---
+++ app/issues/loading.tsx
@@ -0,0 +1,55 @@
+import {
+  Table,
+  TableHeader,
+  TableColumnHeaderCell,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@radix-ui/themes";
+import React from "react";
+import IssueStatusBadge from "../components/IssueStatusBadge";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import IssueActions from "./IssueActions";
+
+const LoadingIssuesPage = () => {
+  const issues = [1, 2, 3, 4, 5];
+  return (
+    <div>
+      <IssueActions />
+      <Table.Root variant="surface">
+        <TableHeader>
+          <Table.Row>
+            <TableColumnHeaderCell>Issue</TableColumnHeaderCell>
+            <TableColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </TableColumnHeaderCell>
+            <TableColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </TableColumnHeaderCell>
+          </Table.Row>
+        </TableHeader>
+        <TableBody>
+          {issues.map((issues) => (
+            <TableRow key={issues}>
+              <TableCell>
+                <Skeleton></Skeleton>
+                <div className="block md:hidden">
+                  <Skeleton />
+                </div>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">
+                <Skeleton />
+              </TableCell>
+              <TableCell className="hidden md:table-cell">
+                <Skeleton />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table.Root>
+    </div>
+  );
+};
+
+export default LoadingIssuesPage;
---
+++ app/issues/page.tsx
@@ -0,0 +1,61 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumnHeaderCell,
+  TableHeader,
+  TableRow,
+} from "@radix-ui/themes";
+
+import Link from "../components/Link";
+
+import prisma from "@/prisma/client";
+import IssueStatusBadge from "../components/IssueStatusBadge";
+import IssueActions from "./IssueActions";
+
+
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
+
+  return (
+    <div>
+      <IssueActions />
+
+      <Table.Root variant="surface">
+        <TableHeader>
+          <Table.Row>
+            <TableColumnHeaderCell>Issue</TableColumnHeaderCell>
+            <TableColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </TableColumnHeaderCell>
+            <TableColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </TableColumnHeaderCell>
+          </Table.Row>
+        </TableHeader>
+        <TableBody>
+          {issues.map((issues) => (
+            <TableRow key={issues.id}>
+              <TableCell>
+                <Link href={`/issues/${issues.id}`}>
+                {issues.title}
+                </Link>
+                <div className="block md:hidden">
+                  <IssueStatusBadge status={issues.status}></IssueStatusBadge>
+                </div>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">
+                <IssueStatusBadge status={issues.status}></IssueStatusBadge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">
+                {issues.createdAt.toDateString()}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table.Root>
+    </div>
+  );
+};
+
+export default IssuesPage;
