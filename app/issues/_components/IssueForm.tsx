5e16ea3f549c9eb93f68cef6388baedf87e41e54
Sumit Pathak <sumitpathak1198@gmail.com>
2024-01-03 21:36:34 +0530

    Build API to update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -9,7 +9,7 @@ import {useForm, Controller} from 'react-hook-form';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { createIssueSchema } from '@/app/validationSchemas';
+import { issueSchema } from '@/app/validationSchemas';
 import {z} from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
@@ -20,13 +20,13 @@ const SimpleMDE = dynamic(() => import('react-simplemde-editor'),
   ssr: false
 });
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 
 const IssueForm = ({issue}: {issue?: Issue}) => {
   const router = useRouter();
   const { register, control, handleSubmit, formState:{errors} } = useForm<IssueFormData>({
-    resolver:zodResolver(createIssueSchema)
+    resolver:zodResolver(issueSchema)
   });
 
   const [error, setError] = useState('');
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
-export const createIssueSchema = z.object({
+export const issueSchema = z.object({
     title: z.string().min(1, 'Tittle is required').max(255),
     description: z.string().min(1, 'Description is required')
 });
+
