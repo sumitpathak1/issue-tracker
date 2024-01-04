ad0fbc1be24dcf48918eabd5c55a41c6be5272cb
Sumit Pathak <sumitpathak1198@gmail.com>
2024-01-04 12:10:17 +0530

    UX Improvements

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,6 +1,7 @@
 import { issueSchema } from "@/app/validationSchemas";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
+import delay from "delay";
 
 export async function PATCH(
     request: NextRequest, 
@@ -30,6 +31,8 @@ export async function PATCH(
     export async function DELETE(
         request: NextRequest, 
         {params}: {params: {id: string}}){
+
+        await delay(2000);
 
         const issue = await prisma.issue.findUnique({
             where: {id:parseInt(params.id)}    
--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Spinner } from '@/app/components'
 import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
@@ -9,12 +10,16 @@ const DeleteIssueButton = ({issueId}: {issueId: number}) => {
 
     const router = useRouter();
     const [error, setError] = useState(false);
+    const [isDeleting, setDeleting] = useState(false);
+
     const deleteIssue = async()=>{
         try {
+            setDeleting(true);
             await axios.delete('/api/issues/'+issueId);
              router.push('/issues');
             router.refresh(); 
         } catch (error) {
+            setDeleting(false);
             setError(true);
         }
            
@@ -27,7 +32,8 @@ const DeleteIssueButton = ({issueId}: {issueId: number}) => {
 
     <AlertDialog.Root>
         <AlertDialog.Trigger>
-        <Button color="red">Delete Issue</Button>
+        <Button color="red" disabled = {isDeleting}>Delete Issue
+        </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
             <AlertDialog.Title>
@@ -42,7 +48,9 @@ const DeleteIssueButton = ({issueId}: {issueId: number}) => {
                 </AlertDialog.Cancel>
             <AlertDialog.Action>
                 <Button color ="red" onClick={deleteIssue}
-                >Delete</Button>
+                >Delete
+                
+                </Button>
             </AlertDialog.Action>
             </Flex>
         </AlertDialog.Content>
